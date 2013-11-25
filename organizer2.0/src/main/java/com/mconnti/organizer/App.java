27bd9f413d8373faf5c89b76e0395d6165c61570
Anderson <Anderson@santos-note>
2013-11-25 18:52:45 +1300

    Starting new version of organizer, this time on JBoss7.

---
+++ organizer2.0/src/main/java/com/mconnti/organizer/App.java
@@ -0,0 +1,13 @@
+package com.mconnti.organizer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
